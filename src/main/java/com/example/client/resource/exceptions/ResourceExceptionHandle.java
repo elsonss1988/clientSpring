4d810d7904ed902f67247b61f44fe53a9bb841f7
Elson Santos <elsonss1988@gmail.com>
2021-11-25 18:50:53 -0300

    add: Controller Advice e README.MD

--- src/main/java/com/example/client/resource/exceptions/ResourceExceptionHandle.java
+++ src/main/java/com/example/client/resource/exceptions/ResourceExceptionHandle.java
@@ -4,11 +4,13 @@ import com.example.client.services.exceptions.DataBaseException;
 import com.example.client.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 
+@ControllerAdvice
 public class ResourceExceptionHandle {
 
     @ExceptionHandler(ResourceNotFoundException.class)
